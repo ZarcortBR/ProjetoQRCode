6ba91edc74d74f2cf41b622595ee690d49d43ad8
Guilherme Robe <107652575+ZarcortBR@users.noreply.github.com>
2023-02-23 20:58:42 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const wrapper = document.querySelector(".wrapper"),
+qrInput = wrapper.querySelector(".form input"),
+generateBtn = wrapper.querySelector(".form button"),
+qrImg = wrapper.querySelector(".qr-code img");
+
+generateBtn = addEventListener("click", () => {
+    let qrValue=qrInput.value;
+    if (!qrValue) return;
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${qrValue}`;
+    console.log(qrValue)
+    qrImg.addEventListener("load",()=>{
+    wrapper.classList.add("active");   
+    })
+
+});
